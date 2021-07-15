a216f348278fa6140bbcb376fa4783e5e657160c
30005209 <30005209@students.southwales.ac.uk>
2021-07-15 20:31:48 +0100

    Hunter follows the player if found otherwise goes to random points on map

--- Assets/HunterAI.cs
+++ Assets/HunterAI.cs
@@ -13,7 +13,16 @@ public class HunterAI : MonoBehaviour
     private Transform _target;
 
     [SerializeField]
-    private float _speed = 200f;
+    private float _maxSpeed;
+
+    [SerializeField]
+    private float _minSpeed = 200f;
+
+    [SerializeField]
+    private float _defaultSpeed;
+
+    [SerializeField]
+    private float _curSpeed = 200f;
 
     [SerializeField]
     private float _nextWaypointDistance = 3.0f;
@@ -28,14 +37,62 @@ public class HunterAI : MonoBehaviour
 
     private Rigidbody2D _rb;
 
+    [SerializeField]
+    private float _maxTime = 10.0f;
+
+    [SerializeField]
+    private float _curTime = 10.0f;
+
+    [SerializeField]
+    private bool _foundPlayer = false;
+
+    [SerializeField]
+    private PatrolPoints _pp;
+
+    [SerializeField]
+    private int _perception;
+
+    [SerializeField]
+    private CircleCollider2D _triggerRange;
+
+    [SerializeField]
+    private float _maxRadius = 3;
+
+    [SerializeField]
+    private float _curRadius;
+
+    [SerializeField]
+    private float _defaultRadius = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _seeker = GetComponent<Seeker>();
         _rb = GetComponent<Rigidbody2D>();
+        _pp = FindObjectOfType<PatrolPoints>();
 
         InvokeRepeating("UpdatePath", 0f, 0.25f);
+
+        _curRadius = _defaultRadius;
+        _curSpeed = _defaultSpeed;
+        _maxSpeed = _defaultSpeed * 1.5f;
+
+    }
+
+    void PickRandomTarget()
+    {
+        PickNewTarget(_pp.GetPointPos(Random.Range(0, _pp.GetLength())));
+    }
+
+    void PickNewTarget(Transform _newTransform)
+    {
+        _target = _newTransform;
+    }
+
+    private void Update()
+    {
+        _curTime -= Time.deltaTime;
     }
 
     void UpdatePath()
@@ -44,7 +101,84 @@ public class HunterAI : MonoBehaviour
         {
             _seeker.StartPath(_rb.position, _target.position, OnPathComplete);
         }
+
+        if(!_foundPlayer)
+        {
+            if(_curTime < 0)
+            {
+                ResetTimer();
+                PickRandomTarget();
+                ResetSpeed();
+            }
+        }
+        else
+        {
+            if(_curTime < 0)
+            {
+                ResetTimer();
+                _curSpeed = _curSpeed * 0.8f;
+            }
+        }
     }
+
+    void ResetTimer()
+    {
+        _curTime = (float)Random.Range(_maxTime / 2, _maxTime);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Movement>() != null)
+        {
+            if (Random.Range(0, 10) - _perception < 0)
+            {
+                PickNewTarget(collision.transform);
+                FoundPlayer();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Movement>() != null && _foundPlayer)
+        {
+            _foundPlayer = false;
+            PickRandomTarget();
+            ResetRange();
+        }
+    }
+
+    void IncreaseRange()
+    {
+        _triggerRange.radius = _maxRadius;
+    }
+
+    void ResetRange()
+    {
+        _triggerRange.radius = _defaultRadius;
+    }
+
+    void ResetSpeed()
+    {
+        _curSpeed = _defaultSpeed;
+    }
+
+    void FoundPlayer()
+    {
+        _foundPlayer = true;
+        _curSpeed = _maxSpeed;
+        IncreaseRange();
+    }
+    
+    void LostPlayer()
+    {
+        _foundPlayer = false;
+        _curSpeed = _defaultSpeed;
+        ResetRange();
+        ResetSpeed();
+    }
+
+
     void OnPathComplete(Path p)
     {
         if(!p.error)
@@ -54,7 +188,6 @@ public class HunterAI : MonoBehaviour
         }
     }
 
-
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -63,7 +196,7 @@ public class HunterAI : MonoBehaviour
             _reachedEndOfPath =_currentWaypoint >= _path.vectorPath.Count;
             
             Vector2 _direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _rb.position).normalized;
-            Vector2 _force = _direction * _speed * Time.deltaTime;
+            Vector2 _force = _direction * _curSpeed * Time.deltaTime;
 
             _rb.AddForce(_force);
 
@@ -74,6 +207,10 @@ public class HunterAI : MonoBehaviour
                 _currentWaypoint++;
             }
         
+        }
+        else
+        {
+            PickRandomTarget();
         }
     }
 }
---
+++ Assets/PatrolPoints.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPoints : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform[] _points;
+
+    private void Start()
+    {
+        _points = GetComponentsInChildren<Transform>();
+    }
+
+    public int GetLength()
+    {
+        return _points.Length;
+    }
+
+    public Transform GetPointPos(int i)
+    {
+        return _points[i];
+    }
+
+}
